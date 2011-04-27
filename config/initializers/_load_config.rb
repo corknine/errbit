33ec220ba041c783007e3a4c95c01df23cbdde9b
anark <anark@comcast.net>
2011-04-26 17:08:23 -0700

    Host

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -18,3 +18,5 @@ end
 (Errbit::Application.config.action_mailer.default_url_options ||= {}).tap do |default|
   default.merge! :host => Errbit::Config.host if default[:host].blank?
 end
+
+ActionMailer::Base.default_url_options[:host] = Errbit::Config.host
